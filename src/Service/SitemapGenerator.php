f0fc6fdf038abfaf6d5f6b1164c1e1829855203f
Igor Lukashov <igor.lukashov@videinfra.com>
2020-05-29 12:21:04 +0300

    Sitemap indexes

--- src/Service/SitemapGenerator.php
+++ src/Service/SitemapGenerator.php
@@ -19,15 +19,25 @@ class SitemapGenerator
     /** @var array */
     private $sources = [];
 
+    /** @var string */
+    private $host;
+
+    /** @var string */
+    private $webPath;
+
     /**
      * SitemapGenerator constructor.
      * @param TwigEngine $templating
      * @param $rootDir
      * @param $path
+     * @param $host
      */
-    public function __construct(TwigEngine $templating, $rootDir, $path)
+    public function __construct(TwigEngine $templating, $rootDir, $path, $host)
     {
         $this->templating = $templating;
+        $this->host = $host;
+        $this->webPath = $path;
+
         if (Kernel::MAJOR_VERSION >= 4 ) {
             $this->path = realpath($rootDir . '/../public') . '/' . $path;
         } else {
@@ -60,5 +70,50 @@ class SitemapGenerator
         ]);
 
         file_put_contents($this->path, $content);
+    }
+
+    /**
+     * @param int $sliceCount
+     */
+    public function generateIndex($sliceCount = 10000)
+    {
+        $sitemaps = [];
+
+        foreach ($this->sources as $index => $source) {
+
+            $items = $source->getItems();
+            $itemsCount = count($items);
+
+            if ($itemsCount > $sliceCount) {
+
+                $chunks = array_chunk($items, $sliceCount);
+                foreach ($chunks as $chunkIndex => $chunk) {
+
+                    $sitemapName = sprintf('%s/sitemap-%s-%s.xml', $this->path, $index, $chunkIndex);
+                    $content = $this->templating->render('OctaveSitemapBundle::sitemap.xml.twig', [
+                        'items' => $chunk
+                    ]);
+
+                    file_put_contents($sitemapName, $content);
+                    $sitemaps[] = sprintf('%s/%s/sitemap-%s-%s.xml', $this->host, $this->webPath, $index, $chunkIndex);
+                }
+            }
+            else {
+
+                $sitemapName = sprintf('%s/sitemap-%s.xml', $this->path, $index);
+                $content = $this->templating->render('OctaveSitemapBundle::sitemap.xml.twig', [
+                    'items' => $items
+                ]);
+
+                file_put_contents($sitemapName, $content);
+                $sitemaps[] = sprintf('%s/%s/sitemap-%s.xml', $this->host, $this->webPath, $index);
+            }
+        }
+
+        $index = $this->templating->render('OctaveSitemapBundle::index.xml.twig', [
+            'sitemaps' => $sitemaps
+        ]);
+
+        file_put_contents($this->path . '/sitemap.xml', $index);
     }
 }
