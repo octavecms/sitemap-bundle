e781a1a1a019e7c8f50bc4a1a69ed561c2460363
Igor Lukashov <igor.lukashov@videinfra.com>
2022-06-15 14:49:37 +0300

    minor fix

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -16,6 +16,7 @@ class GenerateCommand extends Command
 
     public function __construct(SitemapGenerator $sitemapGenerator)
     {
+        parent::__construct();
         $this->sitemapGenerator = $sitemapGenerator;
     }
 
